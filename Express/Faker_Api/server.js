f6251be021eb5482f1cc39f9d414106c4d8fc074
Ayman Yahia <ayman.ya1997@gmail.com>
2021-07-26 11:18:40 +0300

    faker_api

---
+++ Express/Faker_Api/server.js
@@ -0,0 +1,41 @@
+const { json } = require("express");
+const express = require("express");
+const app = express();
+const port = 4000;
+const faker=require("faker")
+ class User{
+     constructor(){
+        this.firstName = faker.name.firstName();
+        this.lastName = faker.name.lastName();
+        this.phoneNumber = faker.phone.phoneNumber();
+        this.email = faker.internet.email();
+        this.password = faker.internet.password();
+     }
+ }
+ class Company {
+    constructor(){
+      this.name = faker.company.companyName();
+      this.address = {
+          street: faker.address.streetAddress(),
+          city: faker.address.city(),
+          state: faker.address.state() ,
+          zipCode: faker.address.zipCode(),
+          country: faker.address.country()
+      };
+    }
+  }
+app.get("/api",(req,res)=>{
+    res.json({message:"up and running!!"})
+})
+app.get("/api/users/new", (req, res) => {
+    res.json( new User() );
+});
+  app.get("/api/companies/new", (req, res) => {
+    res.json( new Company()  );
+});
+app.get("/api/user/company", (req, res) => {
+    res.json({User: new User() , Company: new Company()} );
+});
+
+
+app.listen( port, () => console.log(`Listening on port: ${port}`) );
